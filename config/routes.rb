d082a498feb33a55a421cac80e852395a8c43d25
Viktor Tagil <viktor.tagil@gmail.com>
2022-05-23 22:40:17 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :wines
+  namespace :api do
+    namespace :v1 do
+      get 'wines/index'
+      post 'wines/create'
+      delete 'wines/:id', to: 'wines#destroy'
+    end
+  end
+
+  root 'wines#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
